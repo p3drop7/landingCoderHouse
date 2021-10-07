a9d8478ce83009fa1c4a62539c390e8cbd36f5dc
Pedro Palencia <pedropalencia7@gmail.com>
2021-10-07 01:31:43 -0300

    Header styles and css file added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,27 @@
 import React from 'react'
+import "./Header.css"
 
 function Header() {
     return (
-        <div>
-            <div>CODER<span>SUMMIT21</span></div>
-            <div>|||</div>
+        <div className="headerCointainer">
             
-            <div>
-                <div>&lt; 21_<span>NOV</span>_2021 &gt;</div>
-                <div>SE PARTE DE LO QUE SE VIENE</div>
+            <div className="headerNav">
+                <div className="headerNav__title">CODER<span>SUMMIT21</span></div>
+                <div className="headerNav__navIcon">|||</div>
+            </div>
+            
+            <div className="mainContainer">
+                <div>
+                    <div className="mainContainer__date">&lt; 21_<span>NOV</span>_2021 &gt;</div>
+                    <div className="mainContainer__title">SE PARTE DE LO QUE SE VIENE</div>
+                    <button>Inscribirse</button>
+                </div>
             </div>
 
-            <div>Inscribirse</div>
 
-            <div>
-                <div>FALTAN</div>
-                <div>contador</div>
+            <div className="counterContainer">
+                <div className="counterContainer__title">FALTAN</div>
+                <div className="counterContainer__counter">contador</div>
             </div>
         </div>
     )
