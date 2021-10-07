fead1b2aa1371b3bc22ce26e34d7eb14761dfa76
Pedro Palencia <pedropalencia7@gmail.com>
2021-10-07 01:52:59 -0300

    Banner css and styles added

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import "./Banner.css"
 
 function Banner() {
     return (
-        <div>
-            <div>JOIR OUR TECH STARTUP CONFERENCE 2021</div>
-            <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti dicta consequuntur, debitis maxime corrupti cupiditate aperiam iste fuga! Pariatur tenetur optio inventore itaque voluptatem modi quaerat similique placeat aliquam temporibus!</div>
+        <div className="bannerContainer">
+            <div>
+                <div className="banner__title">JOIR OUR TECH STARTUP CONFERENCE 2021</div>
+                <div className="banner__text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti dicta consequuntur, debitis maxime corrupti cupiditate aperiam iste fuga! Pariatur tenetur optio inventore itaque voluptatem modi quaerat similique placeat aliquam temporibus! Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>
+            </div>
         </div>
     )
 }
