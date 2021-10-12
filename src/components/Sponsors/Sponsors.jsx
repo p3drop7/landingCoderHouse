58d67efda13d170e7a49cc8c808d3fd502d4c017
Pedro Palencia <pedropalencia7@gmail.com>
2021-10-11 21:48:28 -0300

    styles added to sponsors

--- src/components/Sponsors/Sponsors.jsx
+++ src/components/Sponsors/Sponsors.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
+import "./Sponsors.css"
 
 function Sponsors() {
     return (
-        <div>
-            <div>PARTICIPACION EXCLUSIVA DE COMPAÑIAS LIDERES EN LATINOAMERICA</div>
+        <div className="sponsors__container">
+            
+            <div className="sponsors__title">PARTICIPACION EXCLUSIVA DE COMPAÑIAS LIDERES EN LATINOAMERICA</div>
 
-            <div>
-                <div>Compañia</div>
-                <div>Compañia</div>
-                <div>Compañia</div>
-                <div>Compañia</div>
-                <div>Compañia</div>
-                <div>Compañia</div>
+            <div className="sponsors__companies">
+                <div>Rappi</div>
+                <div>Rappi</div>
+                <div>Rappi</div>
+                <div>Rappi</div>
+                <div>Rappi</div>
+                <div>Rappi</div>
+                <div>Rappi</div>
+                <div>Rappi</div>
             </div>
         </div>
     )
